b9b746e1305198c3d027718c707abf651b6af5d8
potaajyu1729 <potaajyu.kosen.jp@gmail.com>
2026-05-08 01:14:51 +0900

    Add Files

--- main.js
+++ main.js
@@ -4,25 +4,24 @@ const canvasCtx = canvasElement.getContext("2d");
 
 async function startCamera() {
   const stream = await navigator.mediaDevices.getUserMedia({
-    video: { width: 1280, height: 720 },
+    video: true,
     audio: false
   });
   videoElement.srcObject = stream;
 
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     videoElement.onloadedmetadata = () => resolve();
   });
 }
 
-function resizeCanvas() {
+function resize() {
   canvasElement.width = videoElement.videoWidth;
   canvasElement.height = videoElement.videoHeight;
 }
 
 const hands = new Hands({
-  locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-  }
+  locateFile: (file) =>
+    `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
 });
 
 hands.setOptions({
@@ -32,32 +31,78 @@ hands.setOptions({
   minTrackingConfidence: 0.7
 });
 
-hands.onResults((results) => {
-  canvasCtx.save();
+hands.onResults(onResults);
+
+function onResults(results) {
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
 
-  canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+    videoElement,
+    0,
+    0,
+    canvasElement.width,
+    canvasElement.height
+  );
 
-  if (results.multiHandLandmarks) {
-    for (const landmarks of results.multiHandLandmarks) {
-      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { lineWidth: 3 });
-      drawLandmarks(canvasCtx, landmarks, { radius: 4 });
-    }
+  if (!results.multiHandLandmarks || results.multiHandLandmarks.length < 2) {
+    return;
   }
 
+  const handsLM = results.multiHandLandmarks;
+
+  const h1 = handsLM[0];
+  const h2 = handsLM[1];
+
+  const p1 = h1[8]; 
+  const p2 = h1[4]; 
+  const p3 = h2[8];
+  const p4 = h2[4];
+
+  const x1 = p1.x * canvasElement.width;
+  const y1 = p1.y * canvasElement.height;
+  const x2 = p2.x * canvasElement.width;
+  const y2 = p2.y * canvasElement.height;
+  const x3 = p3.x * canvasElement.width;
+  const y3 = p3.y * canvasElement.height;
+  const x4 = p4.x * canvasElement.width;
+  const y4 = p4.y * canvasElement.height;
+
+  canvasCtx.save();
+
+  canvasCtx.beginPath();
+  canvasCtx.moveTo(x1, y1);
+  canvasCtx.lineTo(x2, y2);
+  canvasCtx.lineTo(x4, y4);
+  canvasCtx.lineTo(x3, y3);
+  canvasCtx.closePath();
+  canvasCtx.clip();
+
+  canvasCtx.drawImage(
+    videoElement,
+    0,
+    0,
+    canvasElement.width,
+    canvasElement.height
+  );
+
   canvasCtx.restore();
-});
+
+  for (const lm of handsLM) {
+    drawConnectors(canvasCtx, lm, HAND_CONNECTIONS);
+    drawLandmarks(canvasCtx, lm);
+  }
+}
 
 async function main() {
   await startCamera();
-  resizeCanvas();
+  resize();
 
   const camera = new Camera(videoElement, {
     onFrame: async () => {
       await hands.send({ image: videoElement });
     },
-    width: videoElement.videoWidth,
-    height: videoElement.videoHeight
+    width: 1280,
+    height: 720
   });
 
   camera.start();
